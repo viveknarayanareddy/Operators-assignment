410ad5c3c0978e034eb78765554e811b39027098
viveknarayanareddy <kviveknarayanareddy@gmail.com>
2022-08-18 09:13:10 +0530

    assinment

---
+++ bitwise.js
@@ -0,0 +1,14 @@
+var x = true;
+var y = false;
+x &= y
+console.log(x);
+
+var x1 = true;
+var y1 = false;
+x1 ^= y1
+console.log(x);
+
+var x2 = true;
+var y2 = false;
+x2 |= y2
+console.log(x);
---
+++ sub.js
@@ -0,0 +1,4 @@
+var b = 10;
+var a = 20;
+a -= b;
+console.log(a);
